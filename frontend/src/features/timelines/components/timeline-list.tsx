c56a3e3a2c320eec4f394b24d3eefc933c4fb1e9
Márton Spiteller <spitellermarton@gmail.com>
2026-08-07 14:23:02 +0200

    fix(frontend): lint issues with timeline-list component

--- frontend/src/features/timelines/components/timeline-list.tsx
+++ frontend/src/features/timelines/components/timeline-list.tsx
@@ -8,28 +8,25 @@ export function TimelineList() {
       <h2>Timelines</h2>
       <p>Count: {timelines.length}</p>
 
-      {timelines && timelines.length > 0 && (
-        <ul>
-          {timelines.map((item) => (
-            <div id={item.id} className="border-2 p-0.5">
-              <h3>
-                {item.title} ({item.isPublished ? 'public' : 'private'})
-              </h3>
-              {item.events && item.events.length > 0 && (
-                <ul>
-                  {item.events.map((event) => (
-                    <li id={event.id}>
-                      <h4>{event.title}</h4>
-                      <small>{event.date}</small>
-                      <p>{event.description}</p>
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </div>
-          ))}
-        </ul>
-      )}
+      <ul>
+        {timelines.map((item) => (
+          <div key={item.id} className="border-2 p-0.5">
+            <h3>
+              {item.title} ({item.isPublished ? 'public' : 'private'})
+            </h3>
+
+            <ul className="border-t-2">
+              {item.events.map((event) => (
+                <li key={event.id} className="border-l-2 my-2">
+                  <h4>{event.title}</h4>
+                  <small>{event.date}</small>
+                  <p>{event.description}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </ul>
     </section>
   );
 }
