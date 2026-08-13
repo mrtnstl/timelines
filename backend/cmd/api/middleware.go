5812acfbdfb36a8a63e0830a62bc7fa6baa5de24
Márton Spiteller <spitellermarton@gmail.com>
2026-08-13 17:44:52 +0200

    add(backend): verifyTimelineOwnership middleware for application level verification

--- backend/cmd/api/middleware.go
+++ backend/cmd/api/middleware.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"errors"
 	"reflect"
 
 	"github.com/gin-gonic/gin"
+	"github.com/mrtnstl/timelines/internal/store"
 )
 
 func (a *application) validateRequest(requestStruct any) gin.HandlerFunc {
@@ -19,6 +21,33 @@ func (a *application) validateRequest(requestStruct any) gin.HandlerFunc {
 		}
 
 		c.Set(CtxKeyValidatedRequest, req)
+		c.Next()
+	}
+}
+
+func (a *application) verifyTimelineOwnership() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// TODO: get userID from JWT
+		userID := "11111111-1111-1111-1111-111111111111"
+
+		timelineID := c.Param(RouteParamKeyTimelineID)
+		if timelineID == "" {
+			a.badRequestResponse(c, errors.New("timelineID is required"))
+			return
+		}
+
+		_, err := a.store.Timelines.GetByID(c.Request.Context(), timelineID, userID)
+		if err != nil {
+			switch {
+			case errors.Is(err, store.ErrNotFound):
+				a.notFoundResponse(c)
+			default:
+				a.internalServerErrorResponse(c)
+				c.Abort()
+			}
+			return
+		}
+
 		c.Next()
 	}
 }
